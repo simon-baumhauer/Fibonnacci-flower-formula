e330a3b1ab38a57af4c5f38024fe550d0e644f9d
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-02-13 16:35:55 +0400

    drawFlower completed

--- script.js
+++ script.js
@@ -4,27 +4,29 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let positionX = canvas.width / 2;
-let positionY = canvas.height / 2;
-let angle = 0;
+let number = 0;
+let scale = 10;
 
 function drawFlower() {
-    ctx.fillStyle = 'red';
-    ctx.strokeStyle = 'blue';
+    let angle = number * 1;
+    let radius = scale * Math.sqrt(number);
+    let positionX = radius * Math.sin(angle) + canvas.width / 2;
+    let positionY = radius * Math.cos(angle) + canvas.height / 2;
+
+    ctx.fillStyle = 'blue';
+    ctx.strokeStyle = 'pink';
     ctx.lineWidth = 5;
     ctx.beginPath();
     ctx.arc(positionX, positionY, 20, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
+
+    number++;
 }
 
 function animate() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    positionX += 5 * Math.sin(angle);
-    positionY += 5 * Math.cos(angle);
-    angle += 0.1;
     drawFlower();
     requestAnimationFrame(animate);
 }
